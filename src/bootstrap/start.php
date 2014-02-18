c3141ee97ea0f4416369a8afd3fa592e8c0bc39b
Tommy Bergeron <tomshreds@me.com>
2014-02-18 18:09:51 -0500

    no debugbar while on production

--- src/bootstrap/start.php
+++ src/bootstrap/start.php
@@ -26,10 +26,17 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(function ()
 {
-    if (file_exists(__DIR__.'/environment.php'))
-        return require __DIR__.'/environment.php';
-    else
+    if (file_exists(__DIR__.'/environment.php')) {
+        $environment = require __DIR__.'/environment.php';
+
+        if ($environment == 'production')
+            Debugbar::disable();
+
+        return $environment;
+    }
+    else {
         return 'development';
+    }
 });
 
 /*
