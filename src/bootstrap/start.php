7109461ad17451b24849b841b58f0cd4b18e4024
Tommy Bergeron <tomshreds@me.com>
2014-02-18 17:43:41 -0500

    environments config

--- src/bootstrap/start.php
+++ src/bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+    'development' => ['localhost'],
+    'production' => ['freelance.brainpad.org'],
 
 ));
 
