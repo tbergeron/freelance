0f024a563530fed3a96ecf5553eb19efd3869899
Tommy Bergeron <tomshreds@me.com>
2014-02-21 12:24:52 -0500

    fix

--- src/app/views/task/partials/starred_task.blade.php
+++ src/app/views/task/partials/starred_task.blade.php
@@ -1,3 +1,1 @@
-<a href="#" class="starred" data-id="{{ $task->id }}">
-    <i class="glyphicon @if($task->is_starred()) glyphicon-star @else glyphicon-star-empty @endif"></i>
-</a>
+<a href="#" class="starred" data-id="{{ $task->id }}"><i class="glyphicon @if($task->is_starred()) glyphicon-star @else glyphicon-star-empty @endif"></i></a>
