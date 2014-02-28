5f66435a964c09538c4ad9c83259ec1f5816ca93
Tommy Bergeron <tomshreds@me.com>
2014-02-28 10:15:03 -0500

    big buttons

--- src/app/views/task/partials/filter_bar.blade.php
+++ src/app/views/task/partials/filter_bar.blade.php
@@ -1,14 +1,14 @@
 <div class="filter_bar well well-sm">
     <div class="btn-group">
-        <a href="?all=1" class="btn btn-default btn-sm @if($all)active@endif">
+        <a href="?all=1" class="btn btn-default btn-default @if($all)active@endif">
             <div class="hidden-xs">{{ trans('task.all') }}</div>
             <div class="visible-xs"><i class="glyphicon glyphicon-asterisk"></i></div>
         </a>
-        <a href="?closed=1" class="btn btn-default btn-sm @if($closed)active@endif">
+        <a href="?closed=1" class="btn btn-default btn-default @if($closed)active@endif">
             <div class="hidden-xs">{{ trans('task.closed_tasks') }}</div>
             <div class="visible-xs"><i class="glyphicon glyphicon-ban-circle"></i></div>
         </a>
-        <a href="?" class="btn btn-default btn-sm @if(!$all && !$closed)active@endif">
+        <a href="?" class="btn btn-default btn-default @if(!$all && !$closed)active@endif">
             <div class="hidden-xs">{{ trans('task.opened_tasks') }}</div>
             <div class="visible-xs"><i class="glyphicon glyphicon-ok-circle"></i></div>
         </a>
@@ -16,7 +16,7 @@
 
     <div class="pull-right">
         <a href="{{ URL::action('TaskController@getCreate', ((isset($project)) ? ['project_id' => $project->id] : null)) }}"
-           class="btn btn-sm btn-success">
+           class="btn btn-default btn-success">
             <i class="glyphicon glyphicon-plus-sign"></i>
             {{ trans('task.task') }}
         </a>
